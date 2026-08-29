e09c80bfac15b87074189c35999cf2ffc7add214
Mainkill1 <Mainkill1@gmail.com>
2026-08-28 18:42:34 -0700

    ENG-2026-344 Perf: Cache Vulkan blend constants per command buffer
    Signed-off-by: Mainkill1 <Mainkill1@gmail.com>

---
+++ tests/unit/test-xbox-vk-blend-constants.c
@@ -0,0 +1,47 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "hw/xbox/nv2a/pgraph/vk/blend-constants-cache.h"
+
+static void test_first_value_emits(void)
+{
+    PGRAPHVkBlendConstantsCache cache = {0};
+
+    g_assert_true(pgraph_vk_blend_constants_cache_update(&cache, 0));
+    g_assert_true(cache.valid);
+    g_assert_cmphex(cache.guest_color, ==, 0);
+}
+
+static void test_identical_value_skips(void)
+{
+    PGRAPHVkBlendConstantsCache cache = {0};
+
+    g_assert_true(pgraph_vk_blend_constants_cache_update(&cache, 0x12345678));
+    g_assert_false(pgraph_vk_blend_constants_cache_update(&cache, 0x12345678));
+    g_assert_true(pgraph_vk_blend_constants_cache_update(&cache, 0x12345679));
+}
+
+static void test_invalidation_forces_emit(void)
+{
+    PGRAPHVkBlendConstantsCache cache = {0};
+
+    g_assert_true(pgraph_vk_blend_constants_cache_update(&cache, 0x89abcdef));
+    pgraph_vk_blend_constants_cache_invalidate(&cache);
+    g_assert_false(cache.valid);
+    g_assert_true(pgraph_vk_blend_constants_cache_update(&cache, 0x89abcdef));
+}
+
+int main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+    g_test_add_func("/xbox/vulkan/blend-constants/first-value",
+                    test_first_value_emits);
+    g_test_add_func("/xbox/vulkan/blend-constants/identical-value",
+                    test_identical_value_skips);
+    g_test_add_func("/xbox/vulkan/blend-constants/invalidation",
+                    test_invalidation_forces_emit);
+
+    return g_test_run();
+}
