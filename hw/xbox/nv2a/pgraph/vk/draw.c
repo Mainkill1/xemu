6660552a6b6ea6b4aec2c327ab936c8dfe8c11c1
Mainkill1 <Mainkill1@gmail.com>
2026-08-29 21:18:17 -0700

    ENG-2026-450 Use measured exact transient-buffer growth

--- hw/xbox/nv2a/pgraph/vk/draw.c
+++ hw/xbox/nv2a/pgraph/vk/draw.c
@@ -1980,6 +1980,9 @@ typedef struct VertexBufferRemap {
     } map[NV2A_VERTEXSHADER_ATTRIBUTES];
 } VertexBufferRemap;
 
+/* Maximum NV2A vertex attribute width: four 32-bit components. */
+static const VkDeviceSize REMAPPED_VERTEX_BLOCK_ALIGNMENT = 4 * sizeof(float);
+
 static VertexBufferRemap remap_unaligned_attributes(PGRAPHState *pg,
                                                     uint32_t num_vertices)
 {
@@ -2036,8 +2039,10 @@ static VertexBufferRemap remap_unaligned_attributes(PGRAPHState *pg,
     if (remap.attributes) {
         StorageBuffer *buffer = &r->storage_buffers[BUFFER_VERTEX_INLINE_STAGING];
         ensure_buffer_space(pg, BUFFER_VERTEX_INLINE_STAGING,
-                            remap.buffer_space_required, 256);
-        buffer->buffer_offset = ROUND_UP(buffer->buffer_offset, 16);
+                            remap.buffer_space_required,
+                            REMAPPED_VERTEX_BLOCK_ALIGNMENT);
+        buffer->buffer_offset = ROUND_UP(buffer->buffer_offset,
+                                         REMAPPED_VERTEX_BLOCK_ALIGNMENT);
     }
 
     return remap;
@@ -2067,7 +2072,8 @@ static void copy_remapped_attributes_to_inline_buffer(PGRAPHState *pg,
     }
 
     assert(pgraph_vk_buffer_has_space_for(pg, BUFFER_VERTEX_INLINE_STAGING,
-                                          remap.buffer_space_required, 256));
+                                          remap.buffer_space_required,
+                                          REMAPPED_VERTEX_BLOCK_ALIGNMENT));
 
     // FIXME: SIMD memcpy
     // FIXME: Caching
