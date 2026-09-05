aacd8f0ab4ed6458c4af86e8cd1fe2b3e5efb0c9
Mainkill1 <Mainkill1@gmail.com>
2026-09-04 19:31:30 -0700

    ENG-2026-523 Fix: Restore OpenGL texture include order

--- hw/xbox/nv2a/pgraph/gl/texture.c
+++ hw/xbox/nv2a/pgraph/gl/texture.c
@@ -19,6 +19,7 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "qemu/osdep.h"
 #include "qemu/fast-hash.h"
 #include "qemu/log.h"
 #include "hw/xbox/nv2a/nv2a_int.h"
