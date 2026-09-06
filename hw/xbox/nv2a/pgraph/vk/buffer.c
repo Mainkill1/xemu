4f8c2b02279d7d6530ee127a9eaa5b40a30de926
Mainkill1 <Mainkill1@gmail.com>
2026-09-06 06:00:10 -0700

    ENG-2026-523 Fix: Allow zeta scratch buffer to source texture copies

--- hw/xbox/nv2a/pgraph/vk/buffer.c
+++ hw/xbox/nv2a/pgraph/vk/buffer.c
@@ -225,9 +225,12 @@ void pgraph_vk_init_buffers(NV2AState *d)
         .buffer_size = r->storage_buffers[BUFFER_STAGING_DST].buffer_size,
     };
 
+    /* Zeta-to-texture copies reuse this buffer as a transfer source when
+     * depth/stencil compute conversion is unnecessary. */
     r->storage_buffers[BUFFER_COMPUTE_DST] = (StorageBuffer){
         .alloc_info = device_alloc_create_info,
         .usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+                 VK_BUFFER_USAGE_TRANSFER_SRC_BIT |
                  VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
         .buffer_size = BUFFER_LINEAR_SCRATCH_INITIAL_SIZE,
     };
