a9072aaaf17cb6d63be70a9b80f6c90d410dfd66
Mainkill1 <Mainkill1@gmail.com>
2026-09-12 10:01:32 -0700

    Vulkan: directly write fresh vertex pages before first draw use

--- hw/xbox/nv2a/pgraph/vk/perf.c
+++ hw/xbox/nv2a/pgraph/vk/perf.c
@@ -205,6 +205,15 @@ void pgraph_vk_perf_record_vertex_staging_copy(PGRAPHVkState *r,
     }
 }
 
+void pgraph_vk_perf_record_vertex_direct_copy(PGRAPHVkState *r,
+                                               uint64_t bytes)
+{
+    if (r->perf.enabled) {
+        r->perf.vertex_direct_bytes += bytes;
+        r->perf.vertex_direct_copy_count++;
+    }
+}
+
 void pgraph_vk_perf_record_vertex_staging_growth(PGRAPHVkState *r)
 {
     if (r->perf.enabled) {
@@ -357,6 +366,8 @@ void pgraph_vk_perf_frame(PGRAPHVkState *r)
             ",\"staged_bytes_per_guest_frame\":%" PRIu64
             ",\"vertex_staged_bytes_per_guest_frame\":%" PRIu64
             ",\"vertex_staging_copies_per_guest_frame\":%" PRIu64
+            ",\"vertex_direct_bytes_per_guest_frame\":%" PRIu64
+            ",\"vertex_direct_copies_per_guest_frame\":%" PRIu64
             ",\"vertex_staging_capacity_bytes\":%zu"
             ",\"vertex_staging_capacity_growths_per_guest_frame\":%" PRIu64
             ",\"vertex_staging_fallback_finishes_per_guest_frame\":%" PRIu64
@@ -385,6 +396,7 @@ void pgraph_vk_perf_frame(PGRAPHVkState *r)
             submit_count, perf->submit_info_count, perf->command_buffer_count,
             perf->staged_bytes, perf->vertex_staged_bytes,
             perf->vertex_staging_copy_count,
+            perf->vertex_direct_bytes, perf->vertex_direct_copy_count,
             r->storage_buffers[BUFFER_VERTEX_RAM_STAGING].buffer_size,
             perf->vertex_staging_capacity_growth_count,
             perf->vertex_staging_fallback_finish_count,
@@ -417,6 +429,8 @@ void pgraph_vk_perf_frame(PGRAPHVkState *r)
     perf->staged_bytes = 0;
     perf->vertex_staged_bytes = 0;
     perf->vertex_staging_copy_count = 0;
+    perf->vertex_direct_bytes = 0;
+    perf->vertex_direct_copy_count = 0;
     perf->vertex_staging_capacity_growth_count = 0;
     perf->vertex_staging_fallback_finish_count = 0;
     perf->native_bc_upload_count = 0;
--- hw/xbox/nv2a/pgraph/vk/renderer.h
+++ hw/xbox/nv2a/pgraph/vk/renderer.h
@@ -572,6 +572,8 @@ typedef struct PGRAPHVkPerfTelemetry {
     uint64_t staged_bytes;
     uint64_t vertex_staged_bytes;
     uint64_t vertex_staging_copy_count;
+    uint64_t vertex_direct_bytes;
+    uint64_t vertex_direct_copy_count;
     uint64_t vertex_staging_capacity_growth_count;
     uint64_t vertex_staging_fallback_finish_count;
     uint64_t native_bc_upload_count;
@@ -664,6 +666,10 @@ typedef struct PGRAPHVkState {
 
     MemorySyncRequirement vertex_ram_buffer_syncs[NV2A_VERTEXSHADER_ATTRIBUTES];
     size_t num_vertex_ram_buffer_syncs;
+    MemorySyncRequirement pending_vertex_ram_reads[NV2A_VERTEXSHADER_ATTRIBUTES];
+    size_t num_pending_vertex_ram_reads;
+    uint8_t *vertex_ram_read_pages;
+    size_t num_vertex_ram_read_pages;
 
     VkVertexInputAttributeDescription vertex_attribute_descriptions[NV2A_VERTEXSHADER_ATTRIBUTES];
     int vertex_attribute_to_description_location[NV2A_VERTEXSHADER_ATTRIBUTES];
@@ -864,6 +870,8 @@ void pgraph_vk_perf_record_single_time_submit(PGRAPHVkState *r,
                                                uint64_t staged_bytes);
 void pgraph_vk_perf_record_vertex_staging_copy(PGRAPHVkState *r,
                                                 uint64_t bytes);
+void pgraph_vk_perf_record_vertex_direct_copy(PGRAPHVkState *r,
+                                               uint64_t bytes);
 void pgraph_vk_perf_record_vertex_staging_growth(PGRAPHVkState *r);
 void pgraph_vk_perf_record_vertex_staging_fallback(PGRAPHVkState *r);
 void pgraph_vk_perf_record_bc_upload(PGRAPHVkState *r, bool native,
@@ -898,6 +906,8 @@ void pgraph_vk_bind_vertex_attributes_inline(NV2AState *d);
 void pgraph_vk_update_vertex_ram_buffer(PGRAPHState *pg, hwaddr offset, void *data,
                                         VkDeviceSize size);
 void pgraph_vk_update_vertex_ram_buffer_after_surface_readback(
+    PGRAPHState *pg, hwaddr offset, void *data, VkDeviceSize size);
+void pgraph_vk_update_unread_vertex_ram_buffer_after_surface_readback(
     PGRAPHState *pg, hwaddr offset, void *data, VkDeviceSize size);
 VkDeviceSize pgraph_vk_update_index_buffer(PGRAPHState *pg, void *data,
                                            VkDeviceSize size);
