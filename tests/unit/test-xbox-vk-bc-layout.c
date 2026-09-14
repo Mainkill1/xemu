c7ad1218a6b3ce3bbbd3c2703a54cac3c5d04109
Mainkill1 <Mainkill1@gmail.com>
2026-09-13 23:21:29 -0700

    vk: satisfy buffer-to-image copy usage and BC alignment

--- tests/unit/test-xbox-vk-bc-layout.c
+++ tests/unit/test-xbox-vk-bc-layout.c
@@ -29,6 +29,23 @@ static void test_cube_face_alignment(void)
                      NV2A_CUBEMAP_FACE_ALIGNMENT);
 }
 
+static void test_native_bc_copy_offsets_use_block_alignment(void)
+{
+    /* The validation run found a BC3 upload starting at 21848 (8 mod 16). */
+    size_t alignment = pgraph_vk_bc_staging_alignment(4, 16);
+    size_t offset = ROUND_UP((size_t)21848, alignment);
+
+    g_assert_cmpuint(alignment, ==, 16);
+    g_assert_cmpuint(offset, ==, 21856);
+    for (unsigned int level = 0; level < 9; level++) {
+        g_assert_cmpuint(offset % 16, ==, 0);
+        offset += pgraph_vk_bc_mip_size(512 >> level,
+                                        512 >> level, 16);
+    }
+    g_assert_cmpuint(pgraph_vk_bc_staging_alignment(4, 8), ==, 8);
+    g_assert_cmpuint(pgraph_vk_bc_staging_alignment(32, 16), ==, 32);
+}
+
 int main(int argc, char **argv)
 {
     g_test_init(&argc, &argv, NULL);
@@ -38,5 +55,7 @@ int main(int argc, char **argv)
                     test_mip_chain_keeps_tail_blocks);
     g_test_add_func("/xbox/vulkan/bc-layout/cube-alignment",
                     test_cube_face_alignment);
+    g_test_add_func("/xbox/vulkan/bc-layout/native-copy-alignment",
+                    test_native_bc_copy_offsets_use_block_alignment);
     return g_test_run();
 }
