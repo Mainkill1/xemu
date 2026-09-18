c688aea8248837b3db7bd72dedc2606af5ae6676
Mainkill1 <Mainkill1@gmail.com>
2026-09-17 22:01:38 -0700

    vk: reconcile scale-only texture transitions

--- hw/xbox/nv2a/pgraph/vk/renderer.h
+++ hw/xbox/nv2a/pgraph/vk/renderer.h
@@ -777,6 +777,7 @@ typedef struct PGRAPHVkState {
     bool texture_binding_source_is_surface[NV2A_MAX_TEXTURES];
     TextureBinding dummy_texture;
     bool texture_descriptor_publication_pending;
+    bool texture_scale_reconciliation_pending;
     VkFormatProperties *texture_format_properties;
     NativeBCFormatSupport
         native_bc_format_support[NV2A_VK_NATIVE_BC_FORMAT_COUNT];
--- hw/xbox/nv2a/pgraph/vk/shaders.c
+++ hw/xbox/nv2a/pgraph/vk/shaders.c
@@ -1662,6 +1662,8 @@ static void update_shader_uniforms(PGRAPHState *pg, const bool update_stage[])
         psh_changed = apply_uniform_updates(
             &binding->psh.module_info->uniforms, PshUniformInfo,
             binding->psh.uniform_locs, &psh_values, PshUniform__COUNT);
+        pgraph_vk_texture_scale_reconciliation_complete(
+            &r->texture_scale_reconciliation_pending);
 
         r->last_uniform_source_epochs.stage[PGRAPH_UNIFORM_STAGE_PSH] =
             pg->uniform_source_epochs.stage[PGRAPH_UNIFORM_STAGE_PSH];
@@ -1805,7 +1807,7 @@ void pgraph_vk_activate_shaders(PGRAPHState *pg,
 
     bool texture_scale_only =
         !update_stage[PGRAPH_UNIFORM_STAGE_PSH] &&
-        r->texture_descriptor_publication_pending;
+        r->texture_scale_reconciliation_pending;
 
     if (!update_stage[PGRAPH_UNIFORM_STAGE_VSH] &&
         !update_stage[PGRAPH_UNIFORM_STAGE_PSH] && !texture_scale_only) {
@@ -1820,6 +1822,8 @@ void pgraph_vk_activate_shaders(PGRAPHState *pg,
     }
     if (texture_scale_only) {
         bool changed = update_texture_scale_only(r);
+        pgraph_vk_texture_scale_reconciliation_complete(
+            &r->texture_scale_reconciliation_pending);
         r->uniform_stage_dirty[PGRAPH_UNIFORM_STAGE_PSH] |= changed;
         sync_uniform_dirty_summary(r);
         if (!full_update) {
